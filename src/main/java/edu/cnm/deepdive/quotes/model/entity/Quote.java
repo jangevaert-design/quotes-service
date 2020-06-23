a30560d1c17de397d165da342af942200655f1ef
Jan Gevaert <jan.gevaert@gmail.com>
2020-06-23 15:00:38 -0600

    Many to many relationship between tags and quotes

--- src/main/java/edu/cnm/deepdive/quotes/model/entity/Quote.java
+++ src/main/java/edu/cnm/deepdive/quotes/model/entity/Quote.java
@@ -1,6 +1,8 @@
 package edu.cnm.deepdive.quotes.model.entity;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,10 +11,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OrderBy;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.ManyToAny;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.lang.NonNull;
 
@@ -48,6 +54,13 @@ public class Quote {
   @JoinColumn(name = "source_id")
   private Source source;
 
+  @ManyToMany(fetch = FetchType.EAGER,
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @JoinTable(name = "quote_tag", joinColumns = @JoinColumn(name = "quote_id"),
+      inverseJoinColumns = @JoinColumn(name = "tag_id"))
+  @OrderBy("name ASC")
+  public List<Tag> tags = new LinkedList<>();
+
 
   public Long getId() {
     return Id;
@@ -78,5 +91,9 @@ public class Quote {
 
   public void setSource(Source source) {
     this.source = source;
+  }
+
+  public List<Tag> getTags() {
+    return tags;
   }
 }
