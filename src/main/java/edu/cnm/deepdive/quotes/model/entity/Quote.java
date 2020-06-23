cd27fb84f50a6e54a0333ba6554eb4aa1f9876ff
Jan Gevaert <jan.gevaert@gmail.com>
2020-06-23 11:09:31 -0600

    controller method for individual resources.

--- src/main/java/edu/cnm/deepdive/quotes/model/entity/Quote.java
+++ src/main/java/edu/cnm/deepdive/quotes/model/entity/Quote.java
@@ -21,19 +21,19 @@ import org.springframework.lang.NonNull;
 @Entity
 public class Quote {
 
-  @NonNull
+
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "quote_id", nullable = false, updatable = false)
   private Long Id;
 
-  @NonNull
+
   @CreationTimestamp
   @Temporal(TemporalType.TIMESTAMP)
   @Column(nullable = false, updatable = false)
   private Date created;
 
-  @NonNull
+
   @UpdateTimestamp
   @Temporal(TemporalType.TIMESTAMP)
   @Column(nullable = false)
@@ -48,17 +48,17 @@ public class Quote {
   @JoinColumn(name = "source_id")
   private Source source;
 
-  @NonNull
+
   public Long getId() {
     return Id;
   }
 
-  @NonNull
+
   public Date getCreated() {
     return created;
   }
 
-  @NonNull
+
   public Date getUpdated() {
     return updated;
   }
