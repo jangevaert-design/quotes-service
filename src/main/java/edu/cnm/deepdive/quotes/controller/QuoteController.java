9db43e7297481c9fdc08ee24ac1498c1d0c3619c
Jan Gevaert <jan.gevaert@gmail.com>
2020-07-13 10:01:31 -0600

    Starting integration with google OAuth 2.0

--- src/main/java/edu/cnm/deepdive/quotes/controller/QuoteController.java
+++ src/main/java/edu/cnm/deepdive/quotes/controller/QuoteController.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.quotes.controller;
 
 import edu.cnm.deepdive.quotes.model.entity.Quote;
+import edu.cnm.deepdive.quotes.model.entity.Source;
 import edu.cnm.deepdive.quotes.model.entity.Tag;
 import edu.cnm.deepdive.quotes.service.QuoteRepository;
 import edu.cnm.deepdive.quotes.service.SourceRepository;
@@ -10,7 +11,6 @@ import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.server.ExposesResourceFor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -74,6 +73,17 @@ public class QuoteController {
   @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
   public Iterable<Quote> search(@RequestParam(name = "q", required = true) String filter) {
     return quoteRepository.getAllByTextContainingOrderByTextAsc(filter);
+  }
+
+  @PutMapping(value = "/{id:\\d+}/source",
+      consumes = MediaType.APPLICATION_JSON_VALUE, produces =  MediaType.APPLICATION_JSON_VALUE)
+  public Quote putSource(@PathVariable long id, @RequestBody Source source) {
+    Quote quote = get(id);
+    if (source != null && source.getId() != null) {
+      source = sourceRepository.findById(source.getId()).orElseThrow(NoSuchElementException::new);
+    }
+    quote.setSource(source);
+    return quoteRepository.save(quote);
   }
   
 }
--- src/main/java/edu/cnm/deepdive/quotes/view/FlatTag.java
+++ src/main/java/edu/cnm/deepdive/quotes/view/FlatTag.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import org.springframework.lang.NonNull;
 
 
-@JsonPropertyOrder({"id", "created", "updated", "name"})
+@JsonPropertyOrder({"id", "created", "updated", "name", "href"})
 public interface FlatTag {
 
   Long getId();
